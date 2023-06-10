c2f349be100e6b9d2653002309caac8db6f94bb7
François MOINE <fmoine@gatling.io>
2023-06-10 16:46:12 +0200

    fixup! chore: refactor code hierarchy

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
 import App from "./App";
 
 if ("serviceWorker" in navigator && !webpackDevServer) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./service-worker.js").then((registration) => {
-      console.log("SW registered: ", registration);
-    }).catch((registrationError) => {
-      console.log("SW registration failed: ", registrationError);
-    });
+    navigator.serviceWorker.register("./service-worker.js");
   });
 }
 
